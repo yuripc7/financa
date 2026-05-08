a860bfcc24d616adeaa7614428fbd55ea3d0da70
yuripc7 <133939847+yuripc7@users.noreply.github.com>
2026-05-08 14:20:17 -0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'es2022',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+});
